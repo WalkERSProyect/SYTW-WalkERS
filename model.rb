0df74e46876c7d8ebc5b14a869e04a5c2509d05e
Eduardo <eaculed@gmail.com>
2014-12-01 17:25:24 +0000

    Añadida configuracion con DataMapper y solucionado problemas en el modelo

--- model.rb
+++ model.rb
@@ -4,11 +4,12 @@ require 'xmlsimple'
 class Usuarios
   include DataMapper::Resource
 
-  property :username, Text
-  property :nombre, Text
-  property :apellidos, Text
-  property :email, Text
-  property :created_at, datetime
-  property :password, String
+  property :id, 	    Serial
+  property :username,   Text
+  property :password,   String
+  property :nombre,     Text
+  property :apellidos,  Text
+  property :email,      Text
+  property :created_at, DateTime
 
 end
--- walkers.rb
+++ walkers.rb
@@ -1,6 +1,28 @@
 require 'sinatra' 
 require 'sinatra/reloader' if development?
 #require 'uri'
+require 'data_mapper'
+
+# Configuracion en local
+configure :development, :test do
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || 
+                             "sqlite3://#{Dir.pwd}/my_walkers.db" )
+end
+
+# Configuracion para Heroku
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
+
+DataMapper::Logger.new($stdout, :debug)
+DataMapper::Model.raise_on_save_failure = true 
+
+require_relative 'model'
+
+DataMapper.finalize
+
+#DataMapper.auto_migrate!
+DataMapper.auto_upgrade!
 
 enable :sessions
 set :session_secret, '*&(^#234a)'
