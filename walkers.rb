c1ae7def1744952a9f1e7087602b3a245dbac5dc
Sergio <alu0100696615@ull.edu.es>
2014-12-03 18:53:07 +0000

    Modificando el post /

--- walkers.rb
+++ walkers.rb
@@ -2,6 +2,9 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'uri'
 require 'data_mapper'
+require 'pp'
+require 'rubygems'
+
 
 # Configuracion en local
 configure :development, :test do
@@ -36,8 +39,40 @@ get '/' do
   end
 end
 
+
+
 get '/signup' do
+  puts "inside get '/': #{params}"
   erb :signup
+end
+
+post '/' do
+  puts "Esto es / barroncia"
+  puts "inside post '/': #{params}"
+  nom = URI::parse(params[:nombre])
+  #ap = URI::parse(params[:apellidos])
+  #em = URI::parse(params[:email])
+  #username = URI::parse(params[:user])
+  
+  #if (params[:pass1] == params[:pass2]) then
+  #password = URI::parse(params[:pass1])
+  #end
+  puts "Este es el nombre #{nom}"
+  if uri.is_a? URI::HTTP or uri.is_a? URI::HTTPS then
+    puts "hola"
+    begin
+      @objeto = Usuario.first_or_create(:username => params[:username], :nombre => params[:nombre])#, :apellidos => params[:apellidos], :email => params[:email], :password => params [:pass1])
+      puts "Este es el nombre #{nom}"
+      puts "Este es el objeto guardado #{objeto}"
+    rescue Exception => e
+      puts "EXCEPTION!!!!!!!!!!!!!!!!!!!"
+      pp @objeto
+      puts e.message
+    end
+  else
+    logger.info "Error! <#{params[:url]}> is not a valid URL"
+  end
+  redirect '/'
 end
 
 get '/login' do
