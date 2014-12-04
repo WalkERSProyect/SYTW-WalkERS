b32784dddb74f31477c91ed79a08815b53ff2b49
Sergio <alu0100696615@ull.edu.es>
2014-12-04 18:56:50 +0000

    Mejorando test.rb

--- test/test.rb
+++ test/test.rb
@@ -16,21 +16,38 @@ end
 
 
 describe 'Tests de app.rb' do
-  #before :all do
-
-  #end   
-         
+  before :all do
+    @username = 'alu4384'
+    @nombre = 'Sergio'
+    @apellidos = 'Rodríguez'
+    @email = 'Diaz@gmail.es'
+    #@objeto = Usuarios.first_or_create(:username => params[:username], :nombre => params[:nombre], :apellidos => params[:apellidos], :email => params[:email], :password => params[:pass1])
+    @Objeto = Usuarios.first_or_create(:nombre =>'Sergio', :apellidos => 'Díaz', :email => 'Sergio@gmail.com')
+  end
 
   it "Comprobar que va a la index" do
     get '/'
     assert last_response.ok?
   end
 
-
   it "Comprobar texto correcto" do
     get '/'
     assert_match 'WalkERS', last_response.body
   end
-    
-    
+
+  #it "Debe devolver que username alu4384 esta en la base de datos" do
+  #  assert @username, @Objeto.username 
+  #end
+
+  it "Debe devolver que el username es igual" do
+    assert @nombre, @Objeto.nombre
+  end    
+
+  it "Debe devolver que no coincide los apellidos" do
+    assert_not_same(@apellidos, @Objeto.apellidos)
+  end
+
+  it "Debe devolver que no coincide los email" do
+    assert_not_same(@email, @Objeto.email)
+  end   
 end
