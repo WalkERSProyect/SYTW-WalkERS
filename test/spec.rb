a40f6b5c9e6ec8487cba7ba3bc096a4541a4b724
Sergio <alu0100696615@ull.edu.es>
2014-12-18 23:39:59 +0000

    Mejorando test de rspec

--- test/spec.rb
+++ test/spec.rb
@@ -15,50 +15,52 @@ describe "Rspec" do
 
 	it "/ coincidendia de nombre 1" do
 		get '/'
-		last_response.body['Tu Chat']
+		last_response.body['Walkers']
 	end	
 
 	it '/ coincidencia de nombre 2' do
 		get '/'
-		last_response.body['Eduardo']
+		last_response.body['Login']
 	end
 
 	it '/ coincidencia de nombre 3' do
 		get '/'
-		last_response.body['Sergio']
+		last_response.body['Comienza']
 	end
     
     it '/' do
-		post '/'
-		last_response.body['Sergio']
+		post '/signup',  :usuario => "sergio7", :nombre => "sergio" , :apellidos => "díaz", 
+						 :email => "sergio@gmail.com", :pass1 => "1", :pass2=> "1", :imagen => "imagen.jpg"
+		last_response.body['ÚLTIMAS RUTAS AÑADIDAS']
 	end
+#[:usuario], :[:nombre], [:apellidos], [:email], [:pass1],[:imagen]
 
-    it "sesión" do
-       get '/', {}, 'rack.session' => { :usuario => 'Eduardo' }
-       expect(last_response).to be_ok
-    end
+   # it "sesión" do
+      # get '/', {}, 'rack.session' => { :usuario => 'Eduardo' }
+     #  expect(last_response).to be_ok
+    #end
 
 
-	it '/ coincidencia de nombre 3' do
-		get '/'
-		last_response.body['y']
-	end
+	#it '/ coincidencia de nombre 3' do
+	#	get '/'
+	#	last_response.body['y']
+	#end
 
-	it '/chat coincidencia de nombre 3' do
-		get '/'
-		last_response.body['Usuarios conectados']
-	end
+	#it '/chat coincidencia de nombre 3' do
+	#	get '/'
+	#	last_response.body['Usuarios conectados']
+	#end
 
-	it 'post' do
-		post '/', params = {:usuario => 'Sergio'}
-		get '/chat'
-		last_response.body['Diseñado']
-	end
+	#it 'post' do
+	#	post '/', params = {:usuario => 'Sergio'}
+	#	get '/chat'
+	#	last_response.body['Diseñado']
+	#end
 
-	it '/chat' do
-		get '/chat'
-		last_response.body['Bienvenido']
-	end
+	#it '/chat' do
+	#	get '/chat'
+	#	last_response.body['Bienvenido']
+	#end
 
     
 
